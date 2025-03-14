9bd969a14333d6ae99015e346dc7105257fa1d99
brownie-lezz <shirleyjong2@gmail.com>
2025-03-14 19:32:11 +0800

    setting up free Gemini API

---
+++ script.js
@@ -0,0 +1,40 @@
+const chatsContainer = document.querySelector(".chats-container");
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = promptForm.querySelector(".prompt-input");
+
+
+let userMessage = "";
+
+// Function to create message elements
+const createMsgElement = (content, ...classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", ...classes);
+    div.innerHTML = content;
+    return div;
+}
+
+// Handle form submission
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+    userMessage = promptInput.value.trim();
+    if (!userMessage) return;
+
+    promptInput.value = ""; // Correct reset
+
+    // Generate user message HTML and add it to the chat container
+    const userMsgHTML = `<p class="message-text"></p>`;
+    const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
+
+    userMsgDiv.querySelector(".message-text").textContent = userMessage;
+    chatsContainer.appendChild(userMsgDiv);
+
+    setTimeout(() => {
+        // Generate bot message HTML and add it to the chat container after 600ms
+        const botMsgHTML = `<img src="google-gemini-icon.png" class="avatar"><p class="message-text">Thinking...</p>`;
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
+        chatsContainer.appendChild(botMsgDiv);
+        generateResponse();
+    }, 600);
+}
+
+promptForm.addEventListener("submit", handleFormSubmit);
